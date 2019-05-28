29036a2835e11e4d8910f027deea715b6c4ba2ec
alexgaya <alejandrogaya1994@gmail.com>
2019-05-28 20:10:02 +0200

    fix path

--- app/Providers/JwtAuthServiceProvider.php
+++ app/Providers/JwtAuthServiceProvider.php
@@ -13,7 +13,7 @@ class JwtAuthServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        require_once app_path().'/Helpers/JwtAuth.php';
+        require_once base_path('app/Helpers/JwtAuth.php');
     }
 
     /**
